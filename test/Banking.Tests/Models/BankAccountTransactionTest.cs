b80aaa31a805c92ee5567dd89f17bd9f844d4c09
ksa607 <karine.samyn@hogent.be>
2016-06-29 09:55:27 +0200

    Refactor unit test BankAccountTransaction : gebruik van Theory en MemberData

--- test/Banking.Tests/Models/BankAccountTransactionTest.cs
+++ test/Banking.Tests/Models/BankAccountTransactionTest.cs
@@ -10,8 +10,6 @@ namespace Banking.Tests.Models
     {
         private static string _bankAccountNumber = "123-4567890-02";
         private BankAccount _bankAccount;
-        private DateTime _yesterday = DateTime.Today.AddDays(-1);
-        private DateTime _tomorrow = DateTime.Today.AddDays(1);
 
         public BankAccountTransactionTest()
         {
@@ -46,77 +44,38 @@ namespace Banking.Tests.Models
             Assert.Equal(TransactionType.Withdraw, t.TransactionType);
         }
 
-
-        [Fact]
-        public void GetTransactions_NoParameters_ReturnsAllTransactions()
-        {
-            _bankAccount.Deposit(100);
-            _bankAccount.Deposit(100);
-            Transaction[] t = _bankAccount.GetTransactions(null, null).ToArray();
-            Assert.Equal(2, t.Length);
-        }
-
         [Fact]
         public void GetTransactions_NoTransactions_ReturnsEmptyList()
         {
             Transaction[] t = _bankAccount.GetTransactions(null, null).ToArray();
             Assert.Equal(0, t.Length);
         }
-
-        [Fact]
-        public void GetTransactions_WithinAPeriodThatHasTransactions_ReturnsTransactions()
+        
+        public static IEnumerable<Object[]> TestData
         {
-            _bankAccount.Deposit(100);
-            _bankAccount.Deposit(100);
-            Transaction[] t = _bankAccount.GetTransactions(_yesterday, _tomorrow).ToArray();
-            Assert.Equal(2, t.Length);
+            get
+            {
+                DateTime yesterday = DateTime.Today.AddDays(-1);
+                DateTime tomorrow = DateTime.Today.AddDays(1);
+
+                yield return new Object[] { null, null, 2 };//All
+                yield return new Object[] { yesterday, tomorrow, 2 };//WithinAPeriodThatHasTransactions
+                yield return new Object[] { yesterday, yesterday, 0 };//WithinAPeriodThatHasNoTransactions
+                yield return new Object[] { null, tomorrow, 2 };//BeforeADateWithTransactions
+                yield return new Object[] { null, yesterday, 0 };//BeforeADateWithoutTransactions
+                yield return new Object[] { yesterday, null, 2 };//AfterADateWithTransactions
+                yield return new Object[] { tomorrow, null, 0 }; //AfterADateWithoutTransactions
+            }
         }
 
-        [Fact]
-        public void GetTransactions_WithinAPeriodThatHasNoTransactions_ReturnsNoTransactions()
+        [Theory]
+        [MemberData("TestData")]
+        public void GetTransactions_ReturnsTransactions(DateTime? from, DateTime? till, int expected)
         {
             _bankAccount.Deposit(100);
             _bankAccount.Deposit(100);
-            Transaction[] t = _bankAccount.GetTransactions(_yesterday, _yesterday).ToArray();
-            Assert.Equal(0, t.Length);
-        }
-
-        [Fact]
-        public void GetTransactions_BeforeADateWithTransactions_ReturnsTransactions()
-        {
-            _bankAccount.Deposit(100);
-            _bankAccount.Deposit(100);
-            Transaction[] t = _bankAccount.GetTransactions(null, _tomorrow).ToArray();
-            Assert.Equal(2, t.Length);
-        }
-
-        [Fact]
-        public void GetTransactions_BeforeADateWithoutTransactions_ReturnsNoTransactions()
-        {
-            _bankAccount.Deposit(100);
-            _bankAccount.Deposit(100);
-            Transaction[] t = _bankAccount.GetTransactions(null, _yesterday).ToArray();
-            Assert.Equal(0, t.Length);
-        }
-
-        [Fact]
-        public void GetTransactions_AfterADateWithTransactions_ReturnsTransactions()
-        {
-            _bankAccount.Deposit(100);
-            _bankAccount.Deposit(100);
-            List<Transaction> t = _bankAccount.GetTransactions(_yesterday, null).ToList();
-            // vb. gebruik van ToList en de Count property
-            Assert.Equal(2, t.Count);
-        }
-
-        [Fact]
-        public void GetTransactions_AfterADateWithoutTransactions_ReturnsNoTransactions()
-        {
-            _bankAccount.Deposit(100);
-            _bankAccount.Deposit(100);
-            IEnumerable<Transaction> t = _bankAccount.GetTransactions(_tomorrow, null).ToArray();
-            // vb gebruik maken van de extension method Count() op IEnumerable
-            Assert.Equal(0, t.Count());
+            IEnumerable<Transaction> t = _bankAccount.GetTransactions(from, till).ToArray();
+            Assert.Equal(expected, t.Count());
         }
 
 
